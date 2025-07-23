13a180f67d8859fb155c22a1a6a94ad778b0850a
Matt Anderson <manderson8787@gmail.com>
2025-07-24 00:00:26 +0800

    Remove base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace 'repo-name' with your actual repository name
-  base: "/wedding-site/",
 });
